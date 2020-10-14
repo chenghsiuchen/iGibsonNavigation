f16b004ce556cd679fd085211f6da7aa3e2424e5
yuoriyurica <simonchen01224@gmail.com>
2020-10-14 14:20:24 +0800

    add a smooth filter

--- main.py
+++ main.py
@@ -21,13 +21,42 @@ def clothoid_ode_rhs(state, s, kappa0, kappa1):
 def eval_clothoid(x0,y0,theta0, kappa0, kappa1, s):
     return odeint(clothoid_ode_rhs, np.array([x0,y0,theta0]), s, (kappa0, kappa1))
 
+def smooth_filter(p_x, p_y):
+    size = 2 # filter range
+    
+    x = []
+    y = []
+    for i in range(size):
+        x.append(p_x[i])
+        y.append(p_y[i])
+
+    for i in range(size, len(p_x) - size, 1):
+        new_x = 0
+        new_y = 0
+        for j in range(size * 2 + 1):
+            idx = j - size
+            new_x += p_x[i + idx]
+            new_y += p_y[i + idx]
+
+        new_x /= (size * 2 + 1)
+        new_y /= (size * 2 + 1)
+
+        x.append(new_x)
+        y.append(new_y)
+
+    for i in range(size):
+        idx = i - size
+        x.append(p_x[idx])
+        y.append(p_y[idx])
+    return np.array(x), np.array(y)
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
 
     res = 50
 
-    for path in range(1):
+    for path in range(20):
         jsonPath = os.path.join('wp10', str(path) + '.json')
         with open(jsonPath, 'r') as jsonfile:
             data = np.array(json.load(jsonfile))
@@ -48,25 +77,27 @@ if __name__ == '__main__':
         # s_x, s_y = zip(*c)
 
         # BSpline
-        # tck_x, u_x = splprep([p_t, p_x], k=3)
-        # tck_y, u_y = splprep([p_t, p_y], k=3)
-        # u = np.linspace(0, 1, 50)
-        # _, s_x = np.array(splev(u, tck_x))
-        # _, s_y = np.array(splev(u, tck_y))
+        _p_x, _p_y = smooth_filter(p_x, p_y)
+        tck_x, u_x = splprep([p_t, p_x], k=3)
+        tck_y, u_y = splprep([p_t, p_y], k=3)
+        u = np.linspace(0, 1, 50)
+        _, s_x = np.array(splev(u, tck_x))
+        _, s_y = np.array(splev(u, tck_y))
 
         # cubic spline
-        # cs_x = CubicSpline(p_t, p_x)
-        # cs_y = CubicSpline(p_t, p_y)
+        # _p_x, _p_y = smooth_filter(p_x, p_y)
+        # cs_x = CubicSpline(p_t, _p_x)
+        # cs_y = CubicSpline(p_t, _p_y)
         # u = np.linspace(0, p_t[-1], 500)
         # s_x = cs_x(u)
         # s_y = cs_y(u)
 
         # NURBS
-        crv = fitting.interpolate_curve(data.tolist(), 3)
-        u = np.linspace(0, 1, 500)
-        pts = np.array(crv.evaluate_list(u))
-        s_x = pts[:, 0]
-        s_y = pts[:, 1]
+        # crv = fitting.interpolate_curve(data.tolist(), 3)
+        # u = np.linspace(0, 1, 500)
+        # pts = np.array(crv.evaluate_list(u))
+        # s_x = pts[:, 0]
+        # s_y = pts[:, 1]
 
         # clothoid
 
@@ -82,6 +113,7 @@ if __name__ == '__main__':
         ax[0, 0].invert_yaxis()
         ax[0, 0].scatter(s_x, s_y, s=1)
         ax[0, 0].scatter(p_x, p_y, s=1)
+        ax[0, 0].scatter(_p_x, _p_y, s=1)
 
         ax[0, 1].scatter(range(len(s_drad)), s_drad, s=1)
          
